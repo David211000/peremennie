f30d9748ea35fb832b9067b01886ee4e1d1736bf
David211000 <dsrapyan@gmail.com>
2024-08-31 13:11:44 +0300

    Add files via upload

---
+++ peremennie2.py
@@ -0,0 +1,5 @@
+The_number_of_completed_tasks =12
+hours=1.5
+course_name='pyton'
+Time_for_one_task=hours/The_number_of_completed_tasks
+print('курс:',course_name,',всего задач:',The_number_of_completed_tasks,',затрачено часов:',hours,'среднее время выполнения',Time_for_one_task)
